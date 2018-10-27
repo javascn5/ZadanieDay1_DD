43fbda9aac13c82420a804d503f42e1fcd099ffa
Fenki93 <fenki1993@gmail.com>
2018-10-27 12:02:09 +0200

    1-9 zadanie

---
+++ src/com/sda/programowanie1/Main.java
@@ -0,0 +1,168 @@
+package com.sda.programowanie1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //todo zadanie 1
+
+        Scanner sca = new Scanner(System.in);
+//        System.out.println("Wprowadz liczbe");
+//
+//        int a = sca.nextInt();
+//
+//        if (a%2 == 0){
+//            System.out.println("Liczba jest parzysta");
+//        }
+//        else System.out.println("Liczba nie jest parzysta");
+
+        // todo zadanie 2
+
+//        Scanner sca = new Scanner(System.in);
+//        int suma = 0;
+//        for (int i = 0; i < 3; i++){
+//            System.out.println("Wprowadz trzy liczby");
+//            int temp = sca.nextInt();
+//            if (temp%2 == 0){
+//                suma += temp;
+//            }
+//        }
+//
+//        System.out.println("Suma: " + suma );
+
+        // todo zadanie 3
+
+//        int suma = 0;
+//        Scanner sca = new Scanner(System.in);
+//        int[]liczby = new int[5];
+//
+//        for(int i = 0; i< liczby.length; i++){
+//            System.out.print("Podaj liczbe calkowita");
+//            liczby[i] = sca.nextInt();
+//            if(liczby[i] % 2 == 0){
+//                suma += liczby[i];
+//            }else{
+//                suma -= liczby[i];
+//            }
+//        }
+//        System.out.println("suma: " + suma);
+//
+//        for(int i = 0; i < liczby.length; i++){
+//            System.out.println(liczby[i]);
+//        }
+//    }
+
+        // todo zadanie 4
+
+//        int suma = 0;
+//        int srednia;
+//        int [] tablica = new int[10];
+//
+//        for (int i = 0; i < tablica.length; i ++){
+//            tablica[i] = sca.nextInt();
+//            suma += tablica[i];
+//
+//        }
+//        srednia = suma / tablica.length;
+//        System.out.println(suma);
+//        System.out.println(srednia);
+//
+//    }
+
+        //todo Zadanie 5
+
+//        int[] tablica = new int[10];
+//
+//        for (int i = 0; i < tablica.length; i++) {
+//            tablica[i] = sca.nextInt();
+//        }
+//
+//        int smallest = tablica[0];
+//        int biggest = tablica[0];
+//
+//        for (int i = 0; i < tablica.length; i++) {
+//            if (tablica[i] > biggest)
+//                biggest = tablica[i];
+//
+//            else if (tablica[i] < smallest)
+//                smallest = tablica[i];
+//        }
+//
+//        System.out.println("najmniejsze: " + smallest);
+//        System.out.println("najwieksze: " + biggest);
+
+        //todo zadanie 6
+//        int temp;
+//        int suma = 0;
+//        do {
+//            System.out.println("wprowadz liczbe");
+//            temp = sca.nextInt();
+//            suma += temp;}
+//
+//        while (temp != 0);
+//        System.out.println("Suma to: " + suma);
+
+        // todo zadanie 7
+
+
+//        ArrayList lista = new ArrayList();
+//        ArrayList parzyste = new ArrayList();
+//        ArrayList nieParzyste = new ArrayList();
+//       int input = 0;
+//        do {
+//            input = sca.nextInt();
+//            lista.add(input);
+//
+//            if (input%2 == 0){
+//                parzyste.add(input);
+//            }
+//            else {
+//                nieParzyste.add(input);
+//            }
+//        }
+//
+//        while (input != 0);
+//
+//        for (int i = 0; i < lista.size() - 1; i ++){
+//            //System.out.println(lista.get(i));
+//        }
+//
+//        System.out.println("Parzyste: " + parzyste);
+//        System.out.println("Nieparzyste: "+ nieParzyste);
+
+
+        //todo Zadanie 8
+
+//        System.out.println("Napisz cos");
+//        String napis = sca.nextLine();
+//        System.out.println("Napisales: " + napis);
+//        System.out.println("Polowa to: "+ napis.substring(napis.length()/2));
+//        System.out.println("Dlugosc wyrazu to: " + napis.length());
+
+
+        // todo Zadanie 9
+
+//        System.out.println("Wpisz slowo:");
+//
+//        String slowo = sca.nextLine();
+//        String naOdwrot = "";
+//
+//        for(int i = slowo.length()-1; i >= 0; i--) {
+//            naOdwrot += slowo.charAt(i);
+//        }
+//
+//        if(naOdwrot.equalsIgnoreCase(slowo)){
+//            System.out.println("To jest palindrom :)");
+//        }else{
+//            System.out.println("To nie jest palindrom :(");
+//        }
+
+
+
+    }
+}
+
+
+
